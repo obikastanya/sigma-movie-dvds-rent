6992d7147751596dd1ffd310ed62e2d4c499bb9b
obi kastanya <obikastanya@gmail.com>
2022-06-28 20:38:44 +0700

    disabled when stock is empty

--- application/renter/static/js/storeDvd.js
+++ application/renter/static/js/storeDvd.js
@@ -63,7 +63,7 @@ class StoreDvd {
               <p class="card-text">${movie.desc}</p>
               <p class="card-text"> Available : ${movie.available_stock} dvds</p>
               <p class="card-text">Status : On Air</p>
-              <a href="/dvd/rent/${movie.id}" class="btn btn-primary" ${disabledRent}>Rent</a>
+              <a href="/dvd/rent/${movie.id}" class="btn btn-primary  ${disabledRent}">Rent</a>
               <a href=/review/dvd/${movie.id} class="btn btn-warning">Review</a>
             </div>
           </div>
